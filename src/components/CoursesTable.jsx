c6b70c94d12d23bfb0c82bdd9401dd8adcd2e593
S M Mishkatus Saqlain <smmsaqlain@gmail.com>
2024-02-21 13:04:48 +0600

    Fixed postcss error

--- src/components/CoursesTable.jsx
+++ src/components/CoursesTable.jsx
@@ -1,4 +1,19 @@
+import { useState } from "react";
+import { useEffect } from "react";
+
 const CoursesTable = () => {
+
+    // Using useEffect to handle data fetching as side effect
+    useEffect( () => {
+        fetch("/courseData.json")
+        .then(res => res.json())
+        .then(data => setData(data))
+        .catch(err => console.log(err))
+    } , [])
+
+    // Using useState to store fetched data
+    const [data, setData] = useState([])
+    console.log(data)
     return (
         <div>
             
